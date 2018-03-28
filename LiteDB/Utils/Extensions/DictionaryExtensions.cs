fea533dd8780856142726a7aebbd934182c30e34
timl <tim@nerfgames.co.uk>
2018-03-28 16:57:36 +0100

    Modified ConnectionString Parsing to not be crazy with regex

--- LiteDB/Utils/Extensions/DictionaryExtensions.cs
+++ LiteDB/Utils/Extensions/DictionaryExtensions.cs
@@ -31,33 +31,31 @@ namespace LiteDB
 
         public static void ParseKeyValue(this IDictionary<string, string> dict, string connectionString)
         {
-            var s = new StringScanner(connectionString);
-
-            while(!s.HasTerminated)
+            int currentPos = 0;
+            while (currentPos < connectionString.Length)
             {
-                var key = s.Scan(@"(.*?)=", 1).Trim();
-                var value = "";
-                s.Scan(@"\s*");
-
-                if (s.Match("\""))
+                int ePos = connectionString.IndexOf('=', currentPos);
+                if (ePos != -1)
                 {
-                    // read a value inside an string " (remove escapes)
-                    value = s.Scan(@"""((?:\\""|.)*?)""", 1).Replace("\\\"", "\"");
-                    s.Scan(@"\s*;?\s*");
+                    int termPos = connectionString.IndexOf(';', ePos);
+                    var key = connectionString.Substring(currentPos, ePos - currentPos);
+                    key = key.Trim(new char[] { ' ', '\"' });
+                    if (termPos != -1)
+                    {
+                        dict[key] = connectionString.Substring(ePos + 1, termPos - ePos - 1);
+                        currentPos = termPos + 1;
+                    }
+                    else
+                    {
+                        var precleaned = connectionString.Substring(ePos + 1);
+                        dict[key] = precleaned.Trim(new char[] { ' ', '\"' });
+                        break;
+                    }
                 }
                 else
                 {
-                    // read value
-                    value = s.Scan(@"(.*?);\s*", 1).Trim();
-
-                    // read last part
-                    if (value.Length == 0)
-                    {
-                        value = s.Scan(".*").Trim();
-                    }
+                    break;
                 }
-
-                dict[key] = value;
             }
         }
     }
