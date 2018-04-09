d8ff21bd7f6366be61b993dbf00f9ba9b15be638
timl <tim@nerfgames.co.uk>
2018-04-09 12:28:51 +0100

    Try Catch for DB Integrity Check

--- LiteDB/Engine/Engine/Integrity.cs
+++ LiteDB/Engine/Engine/Integrity.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace LiteDB
 {
     public partial class LiteEngine
@@ -7,58 +9,66 @@ namespace LiteDB
         /// </summary>
         public bool IntegrityCheck()
         {
-            bool result = _pager.ValidPage<HeaderPage>(0);
-
-            if (!result)
-                return result;
-
-            var headerPage = _pager.GetPage<HeaderPage>(0);
-            foreach (var pageID in headerPage.CollectionPages.Values)
+            bool result = true;
+            try
             {
-                result &= _pager.ValidPage<CollectionPage>(pageID);
+                result &= _pager.ValidPage<HeaderPage>(0);
 
                 if (!result)
                     return result;
 
-                var collisionPage = _pager.GetPage<CollectionPage>(pageID);
-                for (var i = 0; i < collisionPage.Indexes.Length; i++)
+                var headerPage = _pager.GetPage<HeaderPage>(0);
+                foreach (var pageID in headerPage.CollectionPages.Values)
                 {
-                    var index = collisionPage.Indexes[i];
-                    if (index.HeadNode.IsEmpty)
-                        continue;
-
-                    result &= _pager.ValidPage<IndexPage>(index.HeadNode.PageID);
+                    result &= _pager.ValidPage<CollectionPage>(pageID);
 
                     if (!result)
                         return result;
 
-                    var headNode = _pager.GetPage<IndexPage>(index.HeadNode.PageID);
-
-                    //  loop through indices
-
-                    foreach (var node in headNode.Nodes.Values)
+                    var collisionPage = _pager.GetPage<CollectionPage>(pageID);
+                    for (var i = 0; i < collisionPage.Indexes.Length; i++)
                     {
-                        if (node.DataBlock.IsEmpty)
+                        var index = collisionPage.Indexes[i];
+                        if (index.HeadNode.IsEmpty)
                             continue;
 
-                        result &= _pager.ValidPage<DataPage>(node.DataBlock.PageID);
+                        result &= _pager.ValidPage<IndexPage>(index.HeadNode.PageID);
 
                         if (!result)
                             return result;
 
-                        var dataPage = _pager.GetPage<DataPage>(node.DataBlock.PageID);
-                        foreach (var extend in dataPage.DataBlocks.Values)
+                        var headNode = _pager.GetPage<IndexPage>(index.HeadNode.PageID);
+
+                        //  loop through indices
+
+                        foreach (var node in headNode.Nodes.Values)
                         {
-                            if (extend.ExtendPageID == uint.MaxValue)
+                            if (node.DataBlock.IsEmpty)
                                 continue;
 
-                            result &= _pager.ValidPage<ExtendPage>(extend.ExtendPageID);
+                            result &= _pager.ValidPage<DataPage>(node.DataBlock.PageID);
 
                             if (!result)
                                 return result;
+
+                            var dataPage = _pager.GetPage<DataPage>(node.DataBlock.PageID);
+                            foreach (var extend in dataPage.DataBlocks.Values)
+                            {
+                                if (extend.ExtendPageID == uint.MaxValue)
+                                    continue;
+
+                                result &= _pager.ValidPage<ExtendPage>(extend.ExtendPageID);
+
+                                if (!result)
+                                    return result;
+                            }
                         }
                     }
                 }
+            }
+            catch (Exception)
+            {
+                result = false;
             }
 
             return result;
